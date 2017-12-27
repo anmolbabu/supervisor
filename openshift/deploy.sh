cf9ebcee19dcb63c442d6776601217a774f701d8
Devang Gaur <dgaur@redhat.com>
2017-12-27 18:25:58 +0530

    Parameterise supervisor port and local route

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -6,11 +6,12 @@ oc project
 set +x
 
 function oc_process_apply() {
-  echo -e "\n Processing template - $1 ($2) \n"
-  oc process -f $1 | oc apply -f -
+  echo -e "\n Processing template - $1 ($2)\n"
+  oc process -f $1 --param-file=$2 | oc apply -f -
 }
 
 here=`dirname $0`
 template="${here}/template.yaml"
+envfile="${here}/supervisor.env"
 
-oc_process_apply "$template"
+oc_process_apply "$template" "$envfile"
